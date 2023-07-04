fdbb316e4d7285139acb851ba6754056113468b6
hsadiq <ksadik320@gmail.com>
2023-07-04 15:30:45 +0530

    Creating a TicTacToe Game Board

---
+++ src/com/bl/TicTacToe.java
@@ -0,0 +1,16 @@
+package com.bl;
+
+import java.util.Arrays;
+
+public class TicTacToe {
+
+    private char[] board;
+
+    public TicTacToe(){
+        board = new char[10];
+        Arrays.fill(board, ' ');
+    }
+
+    public static void main(String[] args) {
+    }
+}
