308df111ec8862522d718c6a0a491b2363415895
hsadiq <ksadik320@gmail.com>
2023-07-04 17:10:45 +0530

    Added Toss Method In Game

--- src/com/bl/TicTacToe.java
+++ src/com/bl/TicTacToe.java
@@ -1,6 +1,6 @@
 package com.bl;
-
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -8,31 +8,32 @@ public class TicTacToe {
     private char[] board;
     private char playerLetter;
     private char computerLetter;
+    private boolean isPlayerTurn;
 
-    public TicTacToe(){
+    public TicTacToe() {
         board = new char[10];
         Arrays.fill(board, ' ');
     }
 
-    public void chooseLetter(){
+    public void chooseLetter() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Choose a Letter (X or O): ");
         String letter = sc.nextLine();
-        while (!letter.equals("X") && !letter.equals("O")){
+        while (!letter.equals("X") && !letter.equals("O")) {
             System.out.println("Invalid Input");
             letter = sc.nextLine();
         }
 
         playerLetter = letter.charAt(0);
-        if (playerLetter == 'X'){
+        if (playerLetter == 'X') {
             computerLetter = 'O';
-        }else {
+        } else {
             computerLetter = 'X';
         }
-        System.out.println("You Choose: " + playerLetter + " Computer Get: " + computerLetter);
+        System.out.println("You Choose: " + playerLetter + " Computer Gets: " + computerLetter);
     }
 
-    public void printBoard(){
+    public void printBoard() {
         System.out.println(" " + board[1] + " | " + board[2] + " | " + board[3] + " ");
         System.out.println("---+---+---");
         System.out.println(" " + board[4] + " | " + board[5] + " | " + board[6] + " ");
@@ -40,35 +41,59 @@ public class TicTacToe {
         System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9] + " ");
     }
 
-    public void makeMove(int index, char letter){
+    public void makeMove(int index, char letter) {
         board[index] = letter;
     }
 
-    public boolean isSpaceFree(int index){
+    public boolean isSpaceFree(int index) {
         return board[index] == ' ';
     }
 
-    public void setCell(int index, char symbol){
+    public void setCell(int index, char symbol) {
         board[index] = symbol;
     }
 
     public boolean isBoardFull() {
-        for (int i = 1; i < board.length; i++){
-            if (isSpaceFree(i)){
+        for (int i = 1; i < board.length; i++) {
+            if (isSpaceFree(i)) {
                 return false;
             }
         }
         return true;
     }
-    public void letterPosition(){
+
+    public void letterPosition() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Position To Move (1- 9): ");
+        System.out.println("Enter Position To Move (1-9): ");
         int index = sc.nextInt();
+
+        while (index < 1 || index > 9 || !isSpaceFree(index)) {
+            System.out.println("Invalid position or position already occupied. Please choose a valid position (1-9): ");
+            index = sc.nextInt();
+        }
+
+        makeMove(index, playerLetter);
+        isPlayerTurn = false;
+    }
+
+
+    public void firstToss() {
+        Random random = new Random();
+        int tossResult = random.nextInt(2); // 0 or 1
+
+        if (tossResult == 0) {
+            System.out.println("Heads! You play first.");
+            isPlayerTurn = true;
+        } else {
+            System.out.println("Tails! Computer plays first.");
+            isPlayerTurn = false;
+        }
     }
 
     public static void main(String[] args) {
         TicTacToe game = new TicTacToe();
         game.chooseLetter();
+        game.firstToss();
         game.printBoard();
     }
 }
