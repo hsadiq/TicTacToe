4e727a2b4787d1ae54740ff5b4c221f5c1b44d9b
hsadiq <ksadik320@gmail.com>
2023-07-04 15:46:57 +0530

    Adding choose a letter method in TicTacToe Game

--- src/com/bl/TicTacToe.java
+++ src/com/bl/TicTacToe.java
@@ -1,6 +1,7 @@
 package com.bl;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToe {
 
@@ -13,6 +14,26 @@ public class TicTacToe {
         Arrays.fill(board, ' ');
     }
 
+    public void chooseLetter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choose a Letter (X or O): ");
+        String letter = sc.nextLine();
+        while (!letter.equals("X") && !letter.equals("O")){
+            System.out.println("Invalid Input");
+            letter = sc.nextLine();
+        }
+
+        playerLetter = letter.charAt(0);
+        if (playerLetter == 'X'){
+            computerLetter = 'O';
+        }else {
+            computerLetter = 'X';
+        }
+        System.out.println("You Choose: " + letter);
+    }
+
     public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.chooseLetter();
     }
 }
